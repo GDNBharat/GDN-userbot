d9b73b44983218f7e6b96ca92906570b27121dd1
Chinna op <opchinna742@gmail.com>
2025-01-14 22:06:03 +0530

    Update console.py

--- SHUKLA/console.py
+++ SHUKLA/console.py
@@ -28,10 +28,10 @@ if os.path.exists("Internal"):
    load_dotenv("Internal")
 
 
-API_ID = int(getenv("API_ID", 0))
-API_HASH = getenv("API_HASH", None)
-BOT_TOKEN = getenv("BOT_TOKEN", None)
-STRING_SESSION = getenv("STRING_SESSION", None)
+API_ID = int(getenv("API_ID", "28814836"))
+API_HASH = getenv("API_HASH", "92e5e74b5a5dd0b570f628fa8c23b19d")
+BOT_TOKEN = getenv("BOT_TOKEN", "8090896989:AAHBO5J3r35QOIomrOtHeiorkYDhKYpyD0U")
+STRING_SESSION = getenv("STRING_SESSION", "BQG3rfQAdluoNLg4Cxg7uMmgj4Y_XdTjpbMxcuTgxer1k25_X_hmTrFM2JZUHyLT-7j1PnAldi-1PXT4xIP3Xl_reAspJCjk89fx2xBBmrmKZZ2yzxEBki1nzAx80GvapsUQrXP3aLXbABYsXh9EnDnLOcaGXNUdJpzfa08t5lXTvL3_YR_2-nsvAQhgG54gDu_0mKhsEFBxjkYQUIamgDvP9HtATa8lnaZuFz5z7DmcmH9n4VblACOlJpbNUUFM1030MS7qR18WK8grmf57kNjYmJAIwO05spoGGnPTuB_JvJ776zAho5DuW8CF0K3tNxxY1uCvAEtdioL-TpPCI8lYVLSMBQAAAAHETnMdAA")
 MONGO_DB_URL = getenv("MONGO_DB_URL", None)
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", 0))
 OWNER_ID = int(getenv("OWNER_ID", "6762113050"))
